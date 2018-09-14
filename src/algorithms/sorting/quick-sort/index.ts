71affc2dbdada3b9eea6ff80e3ff3fd75369a15f
linyaqi <mr03web@foxmail.com>
2018-09-14 10:35:26 +0800

    quick-sort

--- src/algorithms/sorting/quick-sort/index.ts
+++ src/algorithms/sorting/quick-sort/index.ts
@@ -1,4 +1,4 @@
-// import comparator from "../../../utils/comparator";
+import comparator from "../../../utils/comparator";
 // import arrayUtils from "../../../utils/arrayUtils";
 
 /**
@@ -7,10 +7,32 @@
  */
 export default function quickSort(originalArray: number[]): number[] {
   const array: number[] = [...originalArray];
-  const length: number = array.length;
-
-  for (let i = 0; i < length; i++) {
-    //
+  if (array.length <= 1) {
+    return array;
   }
-  return array;
+
+  const leftArray = [];
+  const rightArray = [];
+
+  const pivotElement = array.shift();
+  const centerArray = pivotElement ? [pivotElement] : [];
+
+  while (array.length) {
+    const currentElement = array.shift();
+    if (pivotElement && currentElement) {
+      if (comparator.equal(currentElement, pivotElement)) {
+        centerArray.push(currentElement);
+      } else if (comparator.lessThan(currentElement, pivotElement)) {
+        leftArray.push(currentElement);
+      } else {
+        rightArray.push(currentElement);
+      }
+    } 
+
+  }
+
+  const leftArraySorted = quickSort(leftArray);
+  const rightArraySorted = quickSort(rightArray);
+
+  return leftArraySorted.concat(centerArray, rightArraySorted);;
 }
