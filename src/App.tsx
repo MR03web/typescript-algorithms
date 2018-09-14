bdf64483eef4916f9d53e5e87b9a7ffa74913bfc
linyaqi <mr03web@foxmail.com>
2018-09-14 10:02:50 +0800

    bubble-sort

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import shellSort from "./algorithms/sorting/shell-sort";
+import sort from "./algorithms/sorting/bubble-sort";
 
 const arr: number[] = [3, 1, 45, 2, 40, 47, 6, 4, 23, 5];
 class App extends React.Component {
@@ -9,7 +9,7 @@ class App extends React.Component {
         <h3>排序数组</h3>
         <h6>{arr.toString()}</h6>
         <h3>排序结果</h3>
-        {shellSort(arr).map((el: number, i: number) => {
+        {sort(arr).map((el: number, i: number) => {
           return <h6 key={i}>{el}</h6>;
         })}
       </div>
---
+++ src/algorithms/sorting/bubble-sort/index.ts
@@ -0,0 +1,24 @@
+import comparator from "../../../utils/comparator";
+import arrayUtils from "../../../utils/arrayUtils";
+/**
+ * @param {number[]} originalArray
+ * @returns {number[]}
+ */
+export default function bubbleSort(originalArray: number[]): number[] {
+  const array: number[] = [...originalArray];
+  const length: number = array.length;
+  let swapped = false;
+  for (let i = 1; i < length; i++) {
+    swapped = false;
+    for (let y = 0; y < length - i; y++) {
+      if (comparator.greaterThan(array[y], array[y + 1])) {
+        arrayUtils.exchange(array, y, y + 1);
+        swapped = true;
+      }
+    }
+    if (!swapped) {
+      break;
+    }
+  }
+  return array;
+}
